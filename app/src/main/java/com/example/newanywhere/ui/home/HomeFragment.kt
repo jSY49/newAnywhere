c8753ff33d657f41990284bbaadf658d64ba69da
jSY49 <jseung49@gmail.com>
2023-05-23 16:42:35 +0900

    add dynamic button

--- app/src/main/java/com/example/newanywhere/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/newanywhere/ui/home/HomeFragment.kt
@@ -1,13 +1,23 @@
 package com.example.newanywhere.ui.home
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
+import androidx.core.view.marginRight
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.newanywhere.R
+import com.example.newanywhere.Retrofit.AreaCode
+import com.example.newanywhere.Retrofit.Item
 import com.example.newanywhere.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -16,6 +26,7 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var homeViewModel :HomeViewModel
     var areaCodeNum = 0
+    var area = ArrayList<Item>()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,7 +39,7 @@ class HomeFragment : Fragment() {
 
 
         homeViewModel.Arearefresh()
-        setRegionButton()
+        observeAreaCodeNum()
 
         return root
     }
@@ -38,11 +49,37 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
-    fun setRegionButton(){
+    fun observeAreaCodeNum(){
         homeViewModel.AreaCodedata.observe(viewLifecycleOwner, Observer {
             areaCodeNum = it.response.body.totalCount
+            area.addAll(it.response.body.items.item)
             //버튼 생성
-
+            for(i in 0 until areaCodeNum){
+                setRegionButton(i)
+            }
         })
+    }
+    fun setRegionButton( index :Int){
+
+        var btn =  Button(context).apply {
+            val lp = LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            lp.setMargins(10, 5, 10, 5)
+
+            layoutParams = lp
+            background = getDrawable(context,R.drawable.area_button)
+            text = area.get(index).name
+            setOnClickListener {
+                Log.d("HomeFragment","setRegionButton : ${area.get(index)}")
+            }
+        }
+        btn.setPadding(10,0,10,0)
+        binding.linearLayInHScroll.addView(btn)
+    }
+    fun getDPI(dp: Int): Int {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp.toFloat(), resources.displayMetrics)
+            .toInt()
     }
 }
